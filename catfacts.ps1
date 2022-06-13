5c8d9da4914ed4bb8660c4fc53868631b12d0eab
FunScripts <107438764+FunScripts@users.noreply.github.com>
2022-06-14 01:03:24 +0400

    Update catfacts.ps1

--- catfacts.ps1
+++ catfacts.ps1
@@ -2,5 +2,7 @@ Add-Type -AssemblyName System.Speech
 $SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
 $SpeechSynth.Rate = 0
 $CatFact = (ConvertFrom-Json (Invoke-WebRequest -Uri 'https://catfact.ninja/fact?max_length=140')).fact
+msg * Did you know?
 $SpeechSynth.Speak("Did you know?")
+msg * $CatFact
 $SpeechSynth.Speak($CatFact)
