c9ce9b384d2ae5eab7d8825880ee0a310baf9f8e
FunScripts <107438764+FunScripts@users.noreply.github.com>
2022-06-14 01:04:41 +0400

    catfacts.ps1

--- catfacts.ps1
+++ catfacts.ps1
@@ -1,4 +1,3 @@
-Add-Type -AssemblyName System.Speech
 $SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
 $SpeechSynth.Rate = 0
 $CatFact = (ConvertFrom-Json (Invoke-WebRequest -Uri 'https://catfact.ninja/fact?max_length=140')).fact
